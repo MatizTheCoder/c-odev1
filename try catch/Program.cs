ef61de9ee6ece3c5a20705ab8715c4f480b6574b
Emre KARAMAN <matizdrum@gmail.com>
2023-03-07 14:55:55 +0300

    try catch ödevi eklendi

---
+++ try catch/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+    
+
+    // try
+    // {
+    //     Console.WriteLine("Bir sayı giriniz: ");
+    //     int sayi = Convert.ToInt32(Console.ReadLine());
+    //     Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+    // }
+    // catch(Exception ex)
+    // {
+    //     Console.WriteLine("Hata: " + ex.Message.ToString());
+    // }
+    // finally
+    // {
+    //     Console.WriteLine("İşlem Tamamlandı!");
+    // }
+
+    try
+    {
+        // int a = int.Parse(null);
+        // int a = int.Parse("test");
+        int a = int.Parse("-20000000000");
+    }
+    catch (ArgumentNullException ex)
+    {
+        Console.WriteLine("Boş değer girdiniz.");
+        Console.WriteLine(ex);
+    }
+    catch(FormatException ex)
+    {
+        Console.WriteLine("Veri tipi uygun değil");
+        Console.WriteLine(ex);
+    }
+    catch(OverflowException ex)
+    {
+        Console.WriteLine("Büyük ya da küçük rakan girdiniz.");
+        Console.WriteLine(ex);
+    }
+    finally
+    {
+        Console.WriteLine("İşlem tamamlandı :) ");
+    }
