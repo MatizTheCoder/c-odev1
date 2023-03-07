9eb1fa6d206b17c0628826509ab4c4f2899886fb
Emre KARAMAN <matizdrum@gmail.com>
2023-03-07 15:49:07 +0300

    if else ternary

---
+++ if else ternary/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int time = DateTime.Now.Hour;
+
+    if(time <= 18){
+        Console.WriteLine("İyi günler");
+    }
+    else{
+        Console.WriteLine("İyi geceler");
+    }
+
+    string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+
+    Console.WriteLine(sonuc);
