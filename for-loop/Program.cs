6189dd248e5030416c62ae26bf67086af403c8f5
Emre KARAMAN <matizdrum@gmail.com>
2023-03-08 12:05:00 +0300

    for loop

---
+++ for-loop/Program.cs
@@ -0,0 +1,23 @@
+﻿     Console.WriteLine("Bir sayı giriniz.");
+     int sayac = 50; 
+
+         for (int i = 1; i < sayac; i++)
+         {
+             if(i%2 == 1)
+                 Console.WriteLine(i);
+            
+         }
+
+        int tekToplam = 0;
+        int ciftToplam = 0;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if(i%2 == 1)
+            tekToplam += 1;
+            else
+            ciftToplam += 1;
+        }
+
+        Console.WriteLine("Tek Toplam: " + tekToplam);
+        Console.WriteLine("Çift Toplam: " + ciftToplam);
