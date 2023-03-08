7f2f787c2bb048db99bad7c1878b5490c3204868
Emre KARAMAN <matizdrum@gmail.com>
2023-03-08 15:09:38 +0300

    diziler

---
+++ diziler/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] bikes = new string[5];
+
+string[] expAnimals = {"sıçan", "fare", "tavşan"};
+
+int[] dizi;
+dizi = new int[3];
+
+bikes[0] = "yamaha";
+dizi[2] = 26;
+
+Console.WriteLine(bikes[0]);
+Console.WriteLine(dizi[2]);
+Console.WriteLine(expAnimals[1]);
