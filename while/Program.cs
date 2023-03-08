ab540a23b5be5f8404d1f19c81fb40f6da08afea
Emre KARAMAN <matizdrum@gmail.com>
2023-03-08 14:45:09 +0300

    while loop

---
+++ while/Program.cs
@@ -0,0 +1,18 @@
+﻿ Console.Write("Bir Sayı Giriniz: ");
+ int sayi = Convert.ToInt32(Console.ReadLine());
+ int sayac = 1;
+ int toplam = 0;
+
+ while (sayac <= sayi)
+ {
+     toplam += sayac;
+     sayac++;
+ }
+  Console.WriteLine(toplam/sayi);
+
+char charackter = 'a';
+while (charackter < 'z')
+{
+    Console.Write(charackter);
+    charackter++;
+}
