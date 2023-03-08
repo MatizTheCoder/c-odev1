73d41bf104e2b05aeee11798d01a8b3868e80a9a
Emre KARAMAN <matizdrum@gmail.com>
2023-03-08 11:25:56 +0300

    swtich case

---
+++ switch case/Program.cs
@@ -0,0 +1,28 @@
+﻿int month = DateTime.Now.Month;
+
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak ayındasınız");
+        break;
+    case 2:
+        Console.WriteLine("Şubat Ayındasınız");
+        break;
+    case 3:
+        Console.WriteLine("Mart Ayındasınız");
+        break;
+    default:
+        Console.WriteLine("Bir hata oluştu.");
+        break;
+}
+
+switch (month)
+{
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Bahar aylarındasınız.");
+        break;
+    default:
+    break;
+}
