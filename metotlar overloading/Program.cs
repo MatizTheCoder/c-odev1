c880856d38e3bcd5d4a2def5b3ba891cdcdf962e
Emre KARAMAN <matizdrum@gmail.com>
2023-03-18 17:40:48 +0300

    Metot overloading

---
+++ metotlar overloading/Program.cs
@@ -0,0 +1,25 @@
+﻿string sayi = "999";
+int outSayi;
+
+bool sonuc = int.TryParse(sayi, out outSayi);
+if(sonuc)
+{
+    Console.WriteLine("İşlem Başarılı");
+    Console.WriteLine(outSayi);
+}
+else
+{
+    Console.WriteLine("İşlem Başarısız");
+}
+
+Metotlar inst = new Metotlar();
+inst.Topla(4,4, out int toplamSonuc);
+Console.WriteLine(toplamSonuc);
+
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+}
